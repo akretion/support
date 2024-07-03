e2967534583f38bec4de89a22d3dd5897a760607
Sébastien BEAU <sebastien.beau@akretion.com>
2024-07-03 10:33:07 +0200

    project_api_client: create/update partner using sudo, as specific project rule can block it

--- project_api_client/models/partner.py
+++ project_api_client/models/partner.py
@@ -41,7 +41,7 @@ class ResPartner(models.Model):
         """This method will return the local partner used for the support
         If the partner is missing it will be created
         If the partner information are obsolete their will be updated"""
-        partner = self.env["res.partner"].with_context(active_test=False).search(
+        partner = self.env["res.partner"].sudo().with_context(active_test=False).search(
             [("support_uid", "=", str(data["uid"]))]
         )
         # Compatibility v10-v12
@@ -50,7 +50,7 @@ class ResPartner(models.Model):
             update_date = fields.Datetime.from_string(update_date)
         if not partner:
             vals = self._get_support_partner_vals(data)
-            partner = self.env["res.partner"].create(vals)
+            partner = self.env["res.partner"].sudo().create(vals)
         elif partner.support_last_update_date < update_date:
             vals = self._get_support_partner_vals(data)
             partner.write(vals)
