e1b71ad1ad743a0e8a9da425d4f0262d4c9997fc
Florian Mounier <paradoxxx.zero@gmail.com>
2025-03-20 12:37:06 +0100

    [ADD] support

---
+++ support/models/ir_actions.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class IrActions(models.Model):
+    _inherit = "ir.actions.actions"
+
+    @api.model
+    def get_bindings(self, model_name):
+        """Add support action to every model"""
+        res = super().get_bindings(model_name)
+        if self.env.user.has_group("support.akretion_erp_group_customer"):
+            action_id = "support.cross_connect_support"
+            if action_id not in [act.get("xml_id") for act in res["action"]]:
+                res["action"].append(self._for_xml_id(action_id))
+
+        return res
