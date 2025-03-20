45f8e228c958fccc3c4c7a0503ab9a3e294df8a7
Florian Mounier <paradoxxx.zero@gmail.com>
2025-03-20 12:37:06 +0100

    [ADD] support

---
+++ support/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Support",
+    "summary": "Provide Akretion cross connect support integration",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/support",
+    "depends": [
+        "cross_connect_client",
+    ],
+    "data": [
+        "data/cross_connect_server.xml",
+        "data/ir_actions_server.xml",
+        "security/res_groups.xml",
+    ],
+}
