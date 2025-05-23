9d5b0aeb05f95f0c7abbc63688bafe0aac54f974
Florian da Costa <florian.dacosta@akretion.com>
2025-05-23 17:23:01 +0200

    [MIG] migrate support to v18

--- support/__manifest__.py
+++ support/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Support",
     "summary": "Provide cross connect support integration",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion",
     "website": "https://github.com/akretion/support",
