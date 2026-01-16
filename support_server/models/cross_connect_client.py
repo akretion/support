913eb333edaf716ba4d6176a75806af757733811
Florian da Costa <florian.dacosta@akretion.com>
2026-01-16 12:15:52 +0100

    [IMP] Manage task creation or defaut list/kanban view depending on the intent of the client

--- support_server/models/cross_connect_client.py
+++ support_server/models/cross_connect_client.py
@@ -11,16 +11,19 @@ class CrossConnectClient(models.Model):
     def _get_final_redirect_url(self, **params):
         """Redirect to project_customer_access with set form default values."""
         url = super()._get_final_redirect_url(**params)
-
-        redirect_params = {
-            "action": "project_customer_access.action_view_all_task",
-            "view_type": "form",
-            **{
-                f"project.task_default_{field}": params.get(field)
-                for field in ("origin_db", "origin_name", "origin_url")
-            },
-        }
+        redirect_params = {}
         if params.get("bypass"):
             redirect_params["action"] = "project.action_view_task"
+        else:
+            redirect_params["action"] = "project_customer_access.action_view_all_task"
+
+        if params.get("intent") == "new":
+            redirect_params.update({
+                "view_type": "form",
+                **{
+                    f"project.task_default_{field}": params.get(field)
+                    for field in ("origin_db", "origin_name", "origin_url")
+                },
+            })
 
         return f"{url}#{urlencode(redirect_params)}"
