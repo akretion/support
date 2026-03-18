b43f8bac767aac6d7fa1ac691c5e7b297a15d4dc
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-18 08:46:52 +0100

    [IMP] support_server: pre-commit auto fixes

--- support_server/models/cross_connect_client.py
+++ support_server/models/cross_connect_client.py
@@ -1,8 +1,9 @@
 # Copyright 2025 Akretion (http://www.akretion.com).
 # @author Florian Mounier <florian.mounier@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from urllib.parse import urlencode
+
 from odoo import models
-from urllib.parse import urlencode, urlparse
 
 
 class CrossConnectClient(models.Model):
@@ -18,12 +19,14 @@ class CrossConnectClient(models.Model):
             redirect_params["action"] = "project_customer_access.action_view_all_task"
 
         if params.get("intent") == "new":
-            redirect_params.update({
-                "view_type": "form",
-                **{
-                    f"project.task_default_{field}": params.get(field)
-                    for field in ("origin_db", "origin_name", "origin_url")
-                },
-            })
+            redirect_params.update(
+                {
+                    "view_type": "form",
+                    **{
+                        f"project.task_default_{field}": params.get(field)
+                        for field in ("origin_db", "origin_name", "origin_url")
+                    },
+                }
+            )
 
         return f"{url}#{urlencode(redirect_params)}"
